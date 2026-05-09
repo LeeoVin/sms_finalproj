5dd47207fec7e291efca0e80b9bb433c67b5a679
LeeoVin <l.bandiola.542330@umindanao.edu.ph>
2026-05-10 00:19:33 +0800

    Login

--- routes/web.php
+++ routes/web.php
@@ -27,7 +27,7 @@ Route::get('/login', function () {
     return view('auth.login');
 })->name('login');
 
-Route::post('/login', function (Request $request) {dd(session()->all());
+Route::post('/login', function (Request $request) {
 
     $request->validate([
         'username' => 'required|string',
